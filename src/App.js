b73c71be47d8e4a1d4d71e405d345797852e6df2
Suad Abrar <suadabrar44@gmail.com>
2025-02-22 13:41:39 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import GroupList from "./GroupList";
-import UserCard from "./UserCard";
-import Counter from "./Counter";
+import GroupList from "./component/GroupList";
+import UserCard from "./component/UserCard";
+import Counter from "./component/Counter";
+import TaskManagement from "./component/TaskManagement";
 
 function App() {
   return (
     <div>
-      <h2>Welcome to the Group List</h2>
       <GroupList />
       <h2>User Card</h2>
       <UserCard
@@ -16,8 +16,8 @@ function App() {
         backgroundColor="#ecc3c3"
       />
       <UserCard name="Abebe Biru" email="abebe.biru@example.com" age={34} />
-      <h2>Counter Application</h2>
       <Counter />
+      <TaskManagement />
     </div>
   );
 }
--- src/component/Counter.js
+++ src/component/Counter.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import "./Counter.css";
 
 function Counter() {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(0);
   return (
     <div>
+      <h2>Counter Application</h2>
       <h3 className="count">{count}</h3>
       <div className="buttons">
         <button onClick={() => setCount(count + 1)} className="button">
